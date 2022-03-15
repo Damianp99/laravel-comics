e476545142d76ab2685ece246c809afe4343ad78
Damianp99 <damianperez200199@gmail.com>
2022-03-16 00:08:27 +0100

    added all comics with forelse

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,4 +1,21 @@
 @extends('layouts.main')
 @section('content')
-   
+   <div class="content-container">
+    <div class="container">
+        <div class="floated">
+            <span>CURRENT SERIES</span>
+        </div>
+        <div class="cards-container d-flex  flex-wrap">
+            @forelse($comics as $comic)
+            <div class="card d-flex flex-wrap">
+                <img src="{{$comic['thumb']}}" alt="{{$comic['title']}}" >
+                <span>{{$comic['series']}}</span>
+            </div>
+            @empty
+                <p>NON C'è ALCUN CONTENUTO</p>
+            @endforelse
+        </div>
+     
+    </div>
+   </div>
 @endsection
