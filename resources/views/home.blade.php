21196d9b273595e05b390f4667327c3a07c219e4
Damianp99 <damianperez200199@gmail.com>
2022-03-15 15:18:55 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+{{--
+Create un nuovo progetto Laravel, su cui lavoreremo sia oggi che domani.
+Oggi concentratevi sul layout: create un file di layout in cui inserire la struttura comune di tutte le pagine del sito web (tag head, tag body, ...) eventualmente includendo header e footer tramite due partials.
+Create poi una rotta per visualizzare la lista di tutti i fumetti recuperati da un file inserito nella cartella config e abbellite il tutto sfruttando SASS.
+Bonus:
+Create più pagine istituzionali che condividono lo stesso layout
+Suggerimenti
+Non dimentichiamo di installare le dipendenze di front-end con npm install
+Non dimentichiamo di importare i file scss in app.scss
+Non dimentichiamo di importare i file css e js compilati con asset all'interno di blade
+ --}}
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+     <title>DC | HOME</title>
+ </head>
+ <body>
+     
+     
+ </body>
+ </html>
